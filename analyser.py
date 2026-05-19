5da788cf392688384d51a2e2df47ae65e37c3c4f
Mohammed Batliwala <mattah43@gmail.com>
2026-05-19 18:13:29 +0100

    agent integration

---
+++ analyser.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from smolagents import CodeAgent, InferenceClientModel
+
+#api key
+load_dotenv()
+
+#free cloud model hugging face 
+model = InferenceClientModel(model_id="Qwen/Qwen2.5-Coder-32B-Instruct") 
+
+#smart agent
+agent = CodeAgent(tools=[], model=model)
+
+print("\n--- Triggering the AI Agent ---")
+response = agent.run("x")
+print("\nAgent Response:")
+print(response)
+
+
+
+
+
+
+
+
+
+
+
+
+
